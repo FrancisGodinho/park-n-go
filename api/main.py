cd2831374e4ed96550db636afd38df353f5d3529
Ubuntu <ubuntu@ip-172-31-90-243.ec2.internal>
2022-03-13 01:50:17 +0000

    added image stuff to server

--- api/main.py
+++ api/main.py
@@ -8,15 +8,19 @@ import os
 from datetime import datetime
 from typing import List
 
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
+import aiofiles
+
 app = FastAPI()
 
 # Firebase initialization
 # Use the application default credentials
-project_id = ''
-cred = credentials.Certificate('./firebasePrivateKey.json')
-firebase_admin.initialize_app(cred)
+#project_id = ''
+#cred = credentials.Certificate('./firebasePrivateKey.json')
+#firebase_admin.initialize_app(cred)
 
-db = firestore.client()
+#db = firestore.client()
 
 # origins = [
 #    "http://localhost",
@@ -43,3 +47,13 @@ async def Test():
 async def TestPost(val: int = 0):
     return f"testing value {val}"
 
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    async with aiofiles.open("img.png", 'wb') as out_file:
+        content = await file.read()  # async read
+        await out_file.write(content)  # async write
+    return {"filename": file.filename}
+
+@app.get("/view_image")
+async def view_image():
+    return FileResponse("./img.png")
