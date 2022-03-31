2270a9aa0da5a0c60795568fe54ee43727a06ae6
Jerry Xu <jerryxwang9@gmail.com>
2022-03-30 17:20:36 -0700

    Display longitude and latitude. Having issues with text input and updating db

--- app/screens/AdminHomeScreen.tsx
+++ app/screens/AdminHomeScreen.tsx
@@ -17,10 +17,24 @@ const AdminHomeScreen = (props: Props) => {
   const [lotRate, setLotRate] = useState(0);
   const [lotCapacity, setLotCapacity] = useState(0);
   const [curNumCars, setCurNumCars] = useState(0);
+  const [lotLongitude, setLotLongitude] = useState(0);
+  const [lotLatitude, setLotLatitude] = useState(0);
 
   // Note that lotId is null because the admin is not associated with any parking lot
   // Currently using a fixed LOT_ID for this admin
   const { lotName, lotId, setLotName } = useGlobalContext();
+
+  const formik = useFormik({
+    initialValues: {
+      longitude: lotLongitude,
+      latitude: lotLatitude,
+    },
+    enableReinitialize: true,
+    // validationSchema: ProfileSchema,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
 
   useEffect(() => {
     (async () => {
@@ -29,6 +43,9 @@ const AdminHomeScreen = (props: Props) => {
       setLotRate(lotSnap.data()?.rate);
       setLotCapacity(lotSnap.data()?.capacity);
       setCurNumCars(lotSnap.data()?.currentNumCars);
+      setLotLongitude(lotSnap.data()?.longitude);
+      setLotLatitude(lotSnap.data()?.latitude);
+
       console.log("lot rate is: " + lotSnap.data()?.rate);
       console.log("lot capacity is: " + lotSnap.data()?.capacity);
     })();
@@ -57,18 +74,39 @@ const AdminHomeScreen = (props: Props) => {
       await updateDoc(doc(db, "lots", LOT_ID), "capacity", lotCapacity);
     })();
   };
+
+  // Problem: have to enter twice to set any value
+  // Also, the CustomTextInput field is invisible
+  const updateLongitude = (newLongitude: number) => {
+    console.log("Setting longitude!!");
+    setLotLongitude(newLongitude);
+    (async () => {
+      console.log("Updateing longitude in database");
+      await updateDoc(doc(db, "lots", LOT_ID), "longitude", lotLongitude);
+    })();
+  };
+
+  const updateLatitude = (newLatitude: number) => {
+    console.log("Setting latitude!!");
+    setLotLatitude(newLatitude);
+    (async () => {
+      console.log("Updateing latitude in database");
+      await updateDoc(doc(db, "lots", LOT_ID), "latitude", lotLatitude);
+    })();
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={[Headers.h1, styles.h1]}>{lotName}</Text>
       <View style={styles.container}>
         <View style={styles.rate}>
-          <Text style={styles.rateText}>{"Rate:"}</Text>
-          <Text style={[Headers.h2, styles.moneyText]}>${lotRate}/hr</Text>
+          <Text style={styles.itemText}>{"Rate:"}</Text>
+          <Text style={[Headers.h2, styles.itemTextBody]}>${lotRate}/hr</Text>
           <CustomButton disabled={false} text="set rate" onPress={updateRate} />
         </View>
         <View style={styles.capacity}>
-          <Text style={styles.capacityText}>{"Capacity:"}</Text>
-          <Text style={[Headers.h2, styles.capacityTextBody]}>
+          <Text style={styles.itemText}>{"Capacity:"}</Text>
+          <Text style={[Headers.h2, styles.itemTextBody]}>
             {curNumCars}/{lotCapacity}
           </Text>
           <CustomButton
@@ -76,6 +114,32 @@ const AdminHomeScreen = (props: Props) => {
             text="set capacity"
             onPress={updateCapacity}
           />
+        </View>
+        <View style={styles.longitude}>
+          <Text style={styles.itemText}>{"Longitude:"}</Text>
+          <Text style={[Headers.h2, styles.itemTextBody]}>{lotLongitude}</Text>
+          {/* <CustomTextInput
+            formik={formik}
+            name="longitude"
+            placeholder={lotLongitude.toString()}
+            value={formik.values.longitude}
+            onSubmitEditing={() => {
+              updateLongitude(formik.values.longitude);
+            }}
+          /> */}
+        </View>
+        <View style={styles.latitude}>
+          <Text style={styles.itemText}>{"Latitude:"}</Text>
+          <Text style={[Headers.h2, styles.itemTextBody]}>{lotLatitude}</Text>
+          {/* <CustomTextInput
+            formik={formik}
+            name="latitude"
+            placeholder="latitude"
+            value={formik.values.latitude}
+            onSubmitEditing={() => {
+              updateLatitude(formik.values.latitude);
+            }}
+          /> */}
         </View>
         <CustomButton disabled={false} text="Sign Out" onPress={signOut} />
       </View>
@@ -88,15 +152,15 @@ export default AdminHomeScreen;
 const styles = StyleSheet.create({
   h1: { marginTop: 20, marginBottom: 10 },
   lotName: { textAlign: "left", marginTop: 10 },
+  itemText: {
+    color: Colors.white,
+    fontWeight: "bold",
+  },
+  itemTextBody: { color: Colors.lightGray },
   rate: {
     width: "100%",
     flexDirection: "row",
     justifyContent: "space-around",
-  },
-  moneyText: { color: Colors.lightGray },
-  rateText: {
-    color: Colors.white,
-    fontWeight: "bold",
   },
   capacity: {
     width: "100%",
@@ -104,10 +168,17 @@ const styles = StyleSheet.create({
     justifyContent: "space-around",
     marginTop: "5%",
   },
-  capacityTextBody: { color: Colors.lightGray },
-  capacityText: {
-    color: Colors.white,
-    fontWeight: "bold",
+  longitude: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: "5%",
+  },
+  latitude: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: "5%",
   },
   container: {
     flex: 1,
