ec339e2723473b14a9476279bd43af20013e4caa
Francis Godinho <francisgodinho2010@hotmail.com>
2022-04-05 12:44:55 -0700

    finished all form reply tests

--- sw/server/client_tests.cpp
+++ sw/server/client_tests.cpp
@@ -3,9 +3,34 @@
 static int parse_response_tests(){
     return 0;
 }
+
 static int form_reply_tests(){
+    ofstream f;
+    vector<vector<int>> test_vec;
+
+    f.open("form_reply_test1.txt"); 
+    test_vec = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    form_reply(test_vec, f);
+    f.close();
+
+    f.open("form_reply_test2.txt"); 
+    test_vec = {{103, 3422, 5435}, {754, 0, 2}, {0, 0, 1}};
+    form_reply(test_vec, f);
+    f.close();
+
+    f.open("form_reply_test3.txt"); 
+    test_vec = {{103, 3422, 5435, 234}, {754, 0, 2, 0}, {0, 0, 1, 2}, {12,3,45,6}};
+    form_reply(test_vec, f);
+    f.close();
+
+    f.open("form_reply_test4.txt"); 
+    test_vec = {{103, 3422, 5435, 234}, {754, 0, 2, 0}};
+    form_reply(test_vec, f);
+    f.close();
+
     return 0;
 }
+
 int main(){
 
     cout << "Running Client Tests!" << endl;
