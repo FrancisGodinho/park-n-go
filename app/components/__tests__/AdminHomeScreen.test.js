6c6153207f5aae5412c96dcf673b35b129934ae3
Jerry Xu <jerryxwang9@gmail.com>
2022-04-05 12:55:55 -0700

    finally fixed and added react testing library

--- app/components/__tests__/AdminHomeScreen.test.js
+++ app/components/__tests__/AdminHomeScreen.test.js
@@ -1,44 +1,29 @@
-// // import dependencies
-// import * as React from "react";
-
-// // import react-testing methods
-// import { render, fireEvent, waitFor, screen } from "@testing-library/react";
-
-// // add custom jest matchers from jest-dom
-// import "@testing-library/jest-dom";
-// // the component to test
-// import AdminHomeScreen from "../../screens/AdminHomeScreen";
-
-// test("loads and display screen", () => {
-//   render(<AdminHomeScreen />);
-//   const admin_screen = screen.getByTestId("admin-safe-view");
-
-//   // fireEvent.click(screen.getByText("Load Greeting"));
-//   expect(getByTestId("admin-safe-view")).not.toBeNull();
-//   // expect(admin_screen).toHaveTextContent("");
-//   //   expect(admin_screen)
-//   //   expect(screen.getByRole("button")).toBeDisabled();
-// });
-
-// test("test hello", () => {
-//   expect(true).toBeTruthy();
-// });
-
 import React from "react";
 import renderer from "react-test-renderer";
+import { cleanup } from "@testing-library/react-native";
+import { fireEvent, render } from "@testing-library/react";
 
 import AdminHomeScreen from "../../screens/AdminHomeScreen";
 
-describe("<AdminHomeScreen />", () => {
-  // it("has 1 child", async () => {
-  //   const tree = renderer.create(<AdminHomeScreen />).toJSON();
-  //   console.log("children length is");
-  //   console.log(tree.children.length);
-  //   // expect(tree.children.length).toBe(1);
-  // });
+// afterEach(cleanup);
 
+let findTextElement = function (tree, element) {
+  console.warn(tree);
+  return true;
+};
+
+describe("<AdminHomeScreen />", () => {
   it("renders correctly", () => {
     const tree = renderer.create(<AdminHomeScreen />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+  // jest.useFakeTimers();
+
+  it("Find text element", () => {
+    let tree = renderer.create(<AdminHomeScreen />).toJSON();
+    expect(findTextElement(tree, "Rate: ")).toBeDefined();
+    expect(findTextElement(tree, "Capacity: ")).toBeDefined();
+    expect(findTextElement(tree, "Longitude: ")).toBeDefined();
+    expect(findTextElement(tree, "Latitude: ")).toBeDefined();
   });
 });
